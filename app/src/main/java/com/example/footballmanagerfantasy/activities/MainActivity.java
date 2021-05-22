d8b28de6d9545f121e522ee23175c4f77c20e4ab
Murilo Rosa <mrmurilo75@gmail.com>
2021-05-22 11:38:23 +0100

    Continue MainActivity and finish New Game

--- app/src/main/java/com/example/footballmanagerfantasy/activities/MainActivity.java
+++ app/src/main/java/com/example/footballmanagerfantasy/activities/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.footballmanagerfantasy.activities;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.footballmanagerfantasy.databinding.ActivityMainBinding;
 
@@ -20,6 +21,32 @@ public class MainActivity extends Fullscreen {
         // Set up the user interaction to manually show or hide the system UI.
         mContentView.setOnClickListener(view -> toggle());
 
-//        binding.newGame.setOnClickListener(view -> launchNewGameActivity());
+        binding.buttonInboxMain.setOnClickListener(view -> launchInboxActivity());
+
+        binding.buttonNextGameMain.setOnClickListener(view -> nextGame());
+        binding.buttonMarketMain.setOnClickListener(view -> launchMarketActivity());
+        binding.buttonTacticsMain.setOnClickListener(view -> launchTacticsActivity());
+
+        binding.button4Main.setOnClickListener(view -> testToast());
+    }
+
+    private void launchInboxActivity() {
+        Toast.makeText(this, "launchInboxActivity()", Toast.LENGTH_SHORT).show();
+    }
+
+    private void launchMarketActivity() {
+        Toast.makeText(this, "launchMarketActivity()", Toast.LENGTH_SHORT).show();
+    }
+
+    private void launchTacticsActivity() {
+        Toast.makeText(this, "launchTacticsActivity()", Toast.LENGTH_SHORT).show();
+    }
+
+    private void nextGame() {
+        Toast.makeText(this, "Next Game", Toast.LENGTH_SHORT).show();
+    }
+
+    private void testToast() {
+        Toast.makeText(this, "Test Toast", Toast.LENGTH_SHORT).show();
     }
 }
